73bbcf88b77d51d418ca37be1049baa1bff890cc
Junior-Oliveira <franciscoanto@gmail.com>
2019-03-27 23:43:25 -0300

    Instalado autenticação

--- public/js/app.js
+++ public/js/app.js
@@ -49180,8 +49180,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Junior\project-laravel-docker\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Junior\project-laravel-docker\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
