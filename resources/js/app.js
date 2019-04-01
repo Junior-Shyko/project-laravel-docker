c3d9fd151cd0feb67666471317b0a4fb2f7e26ad
Junior-Shyko <franciscoanto@gmail.com>
2019-03-31 23:56:59 -0300

    Alterando para rederização de componentes e inserindo Pnotify

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,30 +5,22 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-//require('./bootstrap');
+require('./bootstrap');
 
-//window.Vue = require('vue');
-/*
+window.Vue = require('vue');
+
+
 Vue.component('cond', require('./components/CondComponent.vue').default);
 Vue.component('InfiniteLoading', require('vue-infinite-loading').default);
 Vue.component('user-corrector', require('./components/UserCorrectorComponent.vue').default);
-Vue.component('example', require('./components/ExampleComponent.vue').default);
-*/
 
-var urlUsers = 'https://jsonplaceholder.typicode.com/users';
-new Vue({
-  el: '#main',
-  created: function () {
-    this.getUsers();
-  },
-  data: {
-    lists: []
-  },
-  methods: {
-    getUsers: function () {
-      axios.get(urlUsers).then(response => {
-        this.lists = response.data
-      });
-    }
-  }
+
+const app = new Vue({
+    el: '#app'
+});
+
+$(document).ready(function () {
+  $("#novoclick").click(function(){
+    alert('jquery');
+  });
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,34 +90,16 @@
      </div>
   </section>
 
-  <div class="album py-5 bg-light"  id="main">
+  <div class="album py-5 bg-light" id="app">
    
-   <div class="container">
-        <div id="main" class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h1>VUEjs y Laravel</h1>
-                    <ul class="list-group">
-                        <li v-for="item in lists" class="list-group-item">                      
-                            @{{ item.name }} <strong>@{{ item.email }}</strong>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-8">
-                    <h1>JSON</h1>
-                    <pre>
-                        @{{ $data | json }}
-                    </pre>
-                </div>
-            </div>
-        </div>
-    </div>
+    <cond></cond>  
     
   </div>
 
 </main>
   
 <script src="{{'js/app.js'}}"></script>
+<script src="{{'js/plugins.js'}}"></script>
 <script>
 $.ajaxSetup({
     headers: {
